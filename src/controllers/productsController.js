c876c6de4638ede4664c0d5ebbe18751f163b981
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2022-09-10 14:10:40 -0300

    actualizado: funcionalidad del create y update

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -11,6 +11,7 @@ controller = {
             const products =  await db.Product.findAll({
                 include: [db.Image]
             });
+            //res.json(products)
             res.render('products/products', {products,toThousand})
         } catch (error) {
             res.json({error: error.message});
@@ -42,8 +43,7 @@ controller = {
             const wheelSizes = await db.WheelSize.findAll();
             const sizes = await db.Size.findAll();
             const shifts = await db.Shift.findAll();
-            const suspentions = await db.Suspension.findAll();
-            //{include: [db.Brake,db.Brand,db.WheelSize,db.Frame,db.Shift,db.Suspension]}    
+            const suspentions = await db.Suspension.findAll();   
             res.render('products/productCreate', {brakes,categories,brands,colors,frames,types,wheelSizes,sizes,shifts,suspentions})
         } catch (error) {
             res.json({error: error.message});
@@ -56,8 +56,8 @@ controller = {
 
             // Validaciones de productos
 
-                //const errors = validationResult(req);
-            //if (errors.isEmpty()) {
+                const errors = validationResult(req);
+            if (errors.isEmpty()) {
                 let imagenes= []
                 const productId = await db.Product.create(product);
                 for(let i = 0 ; i<req.files.length;i++) {
@@ -78,25 +78,52 @@ controller = {
                 }
                 
                 
-            //} else {
-                //if (req.files) {
-                //    let {files} = req;
-                //for (let i = 0 ; i< files.length; i++) {
-                //    fs.unlinkSync(path.resolve(__dirname, '../../public/images/'+files[i].filename))
-                //}
-                //};
-                //res.render('products/productCreate',{errors: errors.mapped(), oldData: req.body});
-            //}
+            } else {
+                if (req.files) {
+                    let {files} = req;
+                for (let i = 0 ; i< files.length; i++) {
+                    fs.unlinkSync(path.resolve(__dirname, '../../public/images/'+files[i].filename))
+                }
+                };
+                const brakes = await db.Brake.findAll();
+                const categories = await db.Category.findAll();
+                const brands = await db.Brand.findAll();
+                const colors = await db.Color.findAll();
+                const frames = await db.Frame.findAll();
+                const types = await db.Type.findAll();
+                const wheelSizes = await db.WheelSize.findAll();
+                const sizes = await db.Size.findAll();
+                const shifts = await db.Shift.findAll();
+                const suspentions = await db.Suspension.findAll();
+                res.render('products/productCreate',{errors: errors.mapped(), oldData: req.body,brakes,categories,brands,colors,frames,types,wheelSizes,sizes,shifts,suspentions});
+            }
         } catch (error) {
             res.json({error: error.message});
         }
         
     },
 
-    edit: (req,res) => { 
-        const id = +req.params.id;
-        const product = productModel.find(id);    
-        res.render('products/productEdit',{product});
+    edit: async (req,res) => { 
+        try {
+            const brakes = await db.Brake.findAll();
+            const categories = await db.Category.findAll();
+            const brands = await db.Brand.findAll();
+            const colors = await db.Color.findAll();
+            const frames = await db.Frame.findAll();
+            const types = await db.Type.findAll();
+            const wheelSizes = await db.WheelSize.findAll();
+            const sizes = await db.Size.findAll();
+            const shifts = await db.Shift.findAll();
+            const suspentions = await db.Suspension.findAll();
+            const idToUpdate = +req.params.id;
+            const product = await db.Product.findByPk(idToUpdate,{
+                include: [db.Brake,db.Brand,db.Image,db.WheelSize,db.Frame,db.Shift,db.Suspension,db.Category,db.Color,db.Size,db.Type]
+            });    
+            res.render('products/productEdit',{product, idToUpdate,brakes,categories,brands,colors,frames,types,wheelSizes,sizes,shifts,suspentions});
+        } catch (error) {
+            res.json({error: error.message});
+        }
+        
     },
 
     delete: (req,res) => {
@@ -113,46 +140,45 @@ controller = {
         res.redirect('/productos');
     },
 
-    update: (req,res) => {
-        let idToUpdate = req.params.id;
-        let dataUpdate = req.body;
+    update: async (req,res) => {
+        try {
+            // Validaciones de productos
 
-        // Validaciones de productos
+            const errors = validationResult(req);
+            if (errors.isEmpty()) {
 
-        const errors = validationResult(req);
-        if (errors.isEmpty()) {
-            const product = productModel.find(idToUpdate);  
-            let imagenes= []
-            for(let i = 0 ; i<req.files.length;i++){
-                imagenes.push(req.files[i].filename)
+                let idToUpdate = req.params.id;
+                let dataUpdate = req.body;
+                let imagenes= []
+                const product = await db.Product.update({
+                    ...dataUpdate,
+                }, {
+                    where: {
+                        id: idToUpdate
+                    }
+                });
+                for(let i = 0 ; i<req.files.length;i++) {
+                    imagenes.push({
+                        fileName: req.files[i].filename,
+                        productId: idToUpdate
+                    })
+                }
+                if (imagenes.length > 0) {
+                    await db.Image.bulkCreate(imagenes)
+                    res.redirect('/productos')
+                }
+            } else {
+                if (req.files) {
+                    let {files} = req;
+                    for (let i = 0 ; i< files.length; i++) {
+                        fs.unlinkSync(path.resolve(__dirname, '../../public/images/'+files[i].filename))
+                    }
+                };
+                res.render('products/productEdit',{errors: errors.mapped(), oldData: req.body, idToUpdate });
             }
-            dataUpdate.image =imagenes.length > 0 ? imagenes : product.image;
-
-            if (imagenes.length > 0 && product.image) {
-                let files = product.image;
-                files = files.filter(image => image != 'default-product-image.png')
-            for (let i = 0 ; i< files.length; i++) {
-                fs.unlinkSync(path.resolve(__dirname, '../../public/images/'+files[i]));
-            }
-            };
-
-            let productUpdate = {
-                id: idToUpdate,
-                ...dataUpdate,
-            }
-            productModel.update(productUpdate);
-            res.redirect('/productos');
-        }else {
-            if (req.files) {
-                let {files} = req;
-            for (let i = 0 ; i< files.length; i++) {
-                fs.unlinkSync(path.resolve(__dirname, '../../public/images/'+files[i].filename))
-            }
-            };
-            res.render('products/productEdit',{errors: errors.mapped(), oldData: req.body, idToUpdate });
+        } catch (error) {
+            res.json({error: error.message});
         }
-
-        
     },
     
     filter: (req,res) => {
--- src/middleware/productEditValidation.js
+++ src/middleware/productEditValidation.js
@@ -2,8 +2,8 @@ const {body} = require('express-validator');
 const path = require('path');
 
 validations = [
-    body('product').notEmpty().withMessage('Debe seleccionar una categoria de producto.'),
-    body('type').notEmpty().withMessage('Debe seleccionar un tipo de producto.'),
+    body('categoryId').notEmpty().withMessage('Debe seleccionar una categoria de producto.'),
+    body('typeId').notEmpty().withMessage('Debe seleccionar un tipo de producto.'),
     body('description').isLength({min:8}).withMessage('Debe completar la descripción y debe tener mínimo 8 caracteres.'),
     body('image').custom((value,{req})=> {
         if(req.files.length > 0){
@@ -20,8 +20,8 @@ validations = [
     }),
     body('price').isFloat({min:0}).withMessage('El precio debe ser un número mayor a 0'),
     body('discount').isInt({max:100}).withMessage('El descuento debe ser un número menor a 100% '),
-    body('brand').notEmpty().withMessage('Debe ingresar la marca del producto'),
-    body('model').notEmpty().withMessage('Debe ingresar el modelo del producto')
+    body('brandId').notEmpty().withMessage('Debe ingresar la marca del producto'),
+    body('modelId').notEmpty().withMessage('Debe ingresar el modelo del producto')
 
 ];
 
