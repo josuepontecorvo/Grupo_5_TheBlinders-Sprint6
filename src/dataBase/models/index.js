7ef64b86355a2d29cadda576567737ea40226f9c
Josue Pontecorvo <josuepontecorvo@gmail.com>
2022-09-22 21:21:34 -0300

    Prueba heroku

--- src/dataBase/models/index.js
+++ src/dataBase/models/index.js
@@ -9,8 +9,8 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (process.env.JAWSDB_MARIA_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
